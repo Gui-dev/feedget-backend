04b2dae30826c81c2334520e1381d42ee7ee56f8
Gui-dev <adrianon10@gmail.com>
2022-06-28 04:01:58 -0300

    feat: add init server

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import 'express-async-errors'
 
+import { routes } from './routes'
+
 const PORT = process.env.PORT || 3333
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`)
