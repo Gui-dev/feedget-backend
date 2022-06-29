c4f9d5505b89656b6d5b39e7a50eaef552b1a1dc
Gui-dev <adrianon10@gmail.com>
2022-06-29 04:15:22 -0300

    feat: add validating fields

--- src/use-cases/SubmitFeedbackUseCase.ts
+++ src/use-cases/SubmitFeedbackUseCase.ts
@@ -16,6 +16,18 @@ export class SubmitFeedbackUseCase {
   ) {}
 
   public async execute ({ type, comment, screenshot }: ISubmitFeedbackUseCaseRequest): Promise<Feedback> {
+    if (!type) {
+      throw new Error('Type is required')
+    }
+
+    if (!comment) {
+      throw new Error('Comment is required')
+    }
+
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format')
+    }
+
     const feedback = await this.feedbackRepository.create({
       type,
       comment,
